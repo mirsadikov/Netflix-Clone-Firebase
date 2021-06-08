792b750742880cc08cfbc85c0ddf3b26b16a4f15
Mirabror Sodikov <mirabror9545@gmail.com>
2021-06-08 18:35:07 +0500

    protected and loggedIn routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import * as ROUTES from './constants/routes';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 import { Home, Browse, SignIn, SignUp } from './pages';
 
 export default function App() {
+  const user = {};
+
   return (
     <Router>
-      <Route exact path={ROUTES.BROWSE}>
-        <Browse />
-      </Route>
-      <Route exact path={ROUTES.SIGN_IN}>
+      <IsUserRedirect
+        exact
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_IN}>
         <SignIn />
-      </Route>
-      <Route exact path={ROUTES.SIGN_UP}>
+      </IsUserRedirect>
+
+      <IsUserRedirect
+        exact
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_UP}>
         <SignUp />
-      </Route>
-      <Route exact path={ROUTES.HOME}>
+      </IsUserRedirect>
+
+      <ProtectedRoute exact user={user} path={ROUTES.BROWSE}>
+        <Browse />
+      </ProtectedRoute>
+
+      <IsUserRedirect
+        exact
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.HOME}>
         <Home />
-      </Route>
+      </IsUserRedirect>
     </Router>
   );
 }
